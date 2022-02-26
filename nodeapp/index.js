6ba30e2305fa2f1c7bbb3229e77b7d7a61fda1a8
Bharadwaj36 <91810040+Bharadwaj36@users.noreply.github.com>
2022-02-26 17:07:07 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome Khaja" }');
+    res.send('{ "response": "This is new fork from Bharadwaj" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Get ready to experience" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Congrats!, There you are!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
