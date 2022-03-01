85eb767b1c913bd0e05e9aa796fcf6ea447a70ff
Bharadwaj36 <91810040+Bharadwaj36@users.noreply.github.com>
2022-03-01 08:42:07 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Today is tuesday and holiday" }');
+    res.send('{ "response": "Today is tuesday and day 3 of week" }');
 });
 
 app.get('/will', function (req, res) {
